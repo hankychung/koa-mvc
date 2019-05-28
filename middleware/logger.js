90ba843a9d512256124665c18a57ff178bdde9d1
Han <hanky_chung@hotmail.com>
2019-05-28 10:35:01 +0800

    add middleware

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+
+module.exports = async (ctx, next) => {
+	const start = new Date()
+	console.log(chalk.yellow(`> ${ctx.method}: ${ctx.path}`))
+	await next()
+	const duration = new Date() - start
+	console.log(chalk[ctx.status == 200 ? 'green' : 'red'](`< status: ${ctx.status}  use: ${duration}s`))
+}
